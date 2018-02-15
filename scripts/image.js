99f5dacab7e2d0c7c24117f044c7fba4569857c6
Benny Fung <cfung@ucsd.edu>
2018-02-14 17:23:19 -0800

    semi-fixed search and profile; restructure file hierarchy

---
+++ scripts/image.js
@@ -0,0 +1,7 @@
+class Image {
+  // object relating an image to its tags
+  constructor(img, tags) {
+    this.img = img;
+    this.tags = tags;
+  }
+}
--- search.js
+++
@@ -1,8 +0,0 @@
-function searchSubmit() {
-  let searchEl = document.getElementById("search-result");
-  searchEl.style.display = "inline";
-
-  let searchQuery = document.getElementById("searchQuery");
-  let searchInput = document.getElementById("search-input").value;
-  searchQuery.innerHTML = "Search results for " + searchInput;
-}
